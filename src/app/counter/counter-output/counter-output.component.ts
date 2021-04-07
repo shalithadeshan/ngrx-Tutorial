dc0738bd5c12c2c4d5527822fd7bdd80d82b4e89
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-04-07 17:14:10 +0530

    initial commit

---
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-output',
+  templateUrl: './counter-output.component.html',
+  styleUrls: ['./counter-output.component.scss']
+})
+export class CounterOutputComponent implements OnInit {
+  @Input() counter;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
